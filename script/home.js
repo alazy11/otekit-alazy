6b39d93ab25f8e124a6e6ee617d4c92006c5f8f1
alazy11 <alazyalhimeari11@gmail.com>
2024-10-07 04:58:14 +0300

    working on main page

--- script/home.js
+++ script/home.js
@@ -1,9 +1,12 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+
 const menu = document.querySelectorAll('.nab-back');
 const header = document.querySelector('#header-top');
 const mobileNav = document.querySelector('#mobile-nav-active');
 const mobileNavBtn = document.querySelector('#menu-mobile-btn');
 const serviceMobileOption = document.querySelector('#service-mobile-option');
 const supportMobileOption = document.querySelector('#support-mobile-option');
+
 
 document.addEventListener('scroll',e=>{
     let scroll = window.scrollY;
@@ -33,3 +36,63 @@ supportMobileOption.addEventListener('click',e=>{
 serviceMobileOption.addEventListener('click',e=>{
     e.currentTarget.classList.toggle('active');
 })
+
+
+
+
+const swiper = new Swiper('.swiper', {
+
+    slidesPerView: 1,
+      spaceBetween: 0,
+      grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+
+      // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    490: {
+      slidesPerView: 2,
+      spaceBetween: 0
+    },
+
+    // when window width is >= 991px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 0
+    },
+
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 0
+    },
+  },
+
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+//   const swiper = document.querySelector('.swiper').swiper;
+
+const swiperele = document.querySelector('.swiper').swiper;
+
+
+
+
+
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html'
+    './index.html',
+    // './card.html'
   ],
   theme: {
     extend: {},
